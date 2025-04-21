ecf47a36ef35bfe30172627fc2a0746e7bc376ee
Ivan <elreizerwtf@gmail.com>
2025-04-21 11:58:23 -0300

    docs: explanation of the type AdminUser

--- leadership-blog/types/user.type.ts
+++ leadership-blog/types/user.type.ts
@@ -1,3 +1,8 @@
+/*
+  We create a custom type for the user object that will be returned from the API.
+  This will represent the Admin user object.
+*/
+
 export type AdminUser = {
   id: string;
   email: string | undefined;
