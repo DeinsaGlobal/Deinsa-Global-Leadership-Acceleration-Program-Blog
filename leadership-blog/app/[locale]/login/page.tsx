1b0d51f24b7a3c620acd29fe6d6903070e7ba3e1
Ivan <elreizerwtf@gmail.com>
2025-04-22 17:50:23 -0300

    docs: header import removed

--- leadership-blog/app/[locale]/login/page.tsx
+++ leadership-blog/app/[locale]/login/page.tsx
@@ -1,11 +1,9 @@
 import { AuthTemplate } from '@/components/template/authtemplate';
 import { LoginForm } from '@/components/organisms/loginform';
-import Header from '@/components/organisms/header';
 
 export default function LoginPage() {
   return (
     <AuthTemplate>
-      <Header />
       <LoginForm />
     </AuthTemplate>
   );
