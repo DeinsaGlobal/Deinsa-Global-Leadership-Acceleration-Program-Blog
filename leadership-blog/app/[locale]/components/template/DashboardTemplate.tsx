57c95dd9036ad6856a58e38b9add53ac97eb3083
kenneth chan <david.kachi18@gmail.com>
2025-04-30 11:20:53 -0400

    fix(dashboard): delete innecesary code comments

--- leadership-blog/app/[locale]/components/template/DashboardTemplate.tsx
+++ leadership-blog/app/[locale]/components/template/DashboardTemplate.tsx
@@ -1,14 +1,5 @@
 import { DashboardTabs } from '../organisms/dashboard/DashboardTabs';
 
-/**
- * A functional component that renders the dashboard template for the administration panel.
- * It includes a header with a title, description, and a sign-out button, as well as dashboard tabs.
- *
- * @param {Object} props - The props object.
- * @param {() => void} props.onSignOut - A callback function triggered when the "Cerrar sesión" button is clicked.
- *
- * @returns {JSX.Element} The rendered dashboard template component.
- */
 export function DashboardTemplate({ onSignOut }: { onSignOut: () => void }) {
   return (
     <div className="container mx-auto px-4 py-8">
