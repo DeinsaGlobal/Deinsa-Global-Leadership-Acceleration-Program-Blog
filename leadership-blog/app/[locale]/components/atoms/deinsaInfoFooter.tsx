85cc704f1ed09ef2b7535de5e040e797102e3066
Ivan <elreizerwtf@gmail.com>
2025-04-30 12:04:03 -0300

    docs: changed image structure

--- leadership-blog/app/[locale]/components/atoms/deinsaInfoFooter.tsx
+++ leadership-blog/app/[locale]/components/atoms/deinsaInfoFooter.tsx
@@ -1,22 +1,31 @@
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
 
 export default function DeinsaInfoFooter() {
   const { theme } = useTheme();
   const t = useTranslations('Footer');
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const logoSrc =
     theme === 'dark' ? '/white-deinsa-logo.png' : '/orange-deinsa-logo.png';
   return (
     <div className="flex flex-col items-center justify-center bg-[#F5F5F5] px-8 py-4 dark:bg-[#484848]">
-      <Image
-        src={logoSrc}
-        alt="Deinsa Global logo"
-        width={100}
-        height={100}
-        className="rounded"
-        priority
-      />
+      {mounted && (
+        <Image
+          src={logoSrc}
+          alt="Deinsa Global logo"
+          width={100}
+          height={100}
+          className="rounded"
+          priority
+        />
+      )}
       <p className="text-center">{t('deinsaDescription')}</p>
     </div>
   );
