36a5dc392f8341355469e2e99aba73a86b88adb4
Ivan <elreizerwtf@gmail.com>
2025-05-07 16:49:47 -0300

    feat: banner mainpage

--- leadership-blog/app/[locale]/components/molecules/mainPage/banner.tsx
+++ leadership-blog/app/[locale]/components/molecules/mainPage/banner.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
+import ButtonBanner from '@/components/atoms/mainPage/buttonBanner';
+import { useTranslations } from 'next-intl';
 
 export default function Banner() {
+  const t = useTranslations('Banner');
   return (
-    <div className="relative flex h-[400px] w-full items-center justify-center bg-cover bg-center bg-no-repeat">
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+    <div className="relative mx-auto my-6 flex h-[250px] w-full max-w-screen-xl items-center justify-start overflow-hidden rounded-md bg-gradient-to-r from-blue-500 to-purple-500 p-4">
       <Image
         src="/banner-img.jpg"
         alt="Banner"
-        className="object-cover"
+        className="object-cover brightness-75"
         layout="fill"
         priority
       />
       <div className="relative z-10 text-white">
-        <h1 className="text-4xl font-bold">Welcome to Our Leadership Blog</h1>
-        <p className="mt-4 text-lg">
-          Explore insights, strategies, and stories from the world of
-          leadership.
-        </p>
+        <h1 className="text-4xl font-bold">{t('title')}</h1>
+        <p className="mt-4 text-lg">{t('description')}</p>
+        <ButtonBanner />
       </div>
     </div>
   );
