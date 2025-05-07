3d97643d8db512174c5e6cf555be22b8a491473c
Ivan <elreizerwtf@gmail.com>
2025-05-07 18:29:14 -0300

    wip: departments cards and grid in mainpage

--- leadership-blog/app/[locale]/mainpage/page.tsx
+++ leadership-blog/app/[locale]/mainpage/page.tsx
@@ -2,12 +2,33 @@
 
 import Header from '@/components/organisms/header';
 import Banner from '@/components/molecules/mainPage/banner';
+import DepartmentsSection from '@/components/organisms/mainPage/departmentsSection';
 
+interface DepartmentData {
+  iconSrc: string;
+  altText: string;
+  title: string;
+  description: string;
+  link: string;
+}
+
+const departments: DepartmentData[] = [
+  {
+    iconSrc: '/icons/research-icon.png',
+    altText: 'R&D Icon',
+    title: 'Research & Development',
+    description: 'Bla',
+    link: '/department1',
+  },
+
+  // Add more departments as needed
+];
 export default function MainPage() {
   return (
     <div>
       <Header />
       <Banner />
+      <DepartmentsSection departments={departments} />
     </div>
   );
 }
